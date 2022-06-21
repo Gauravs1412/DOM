6bb01eb77875943bc8b13488ab425410784f819b
gaurav <gauravradadiya1412@gmail.com>
2022-06-21 21:18:59 +0530

    first commit

---
+++ dom.js
@@ -0,0 +1,24 @@
+// // examine the document object
+// // console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype)
+// // console.log(document.head)
+// // console.log(document.all);
+// console.log(document.forms)
+// console.log(document.links)
+
+//getelementbyId
+
+// console.log(document.getElementById('header-title'));
+let headertitle = document.getElementById('header-title')
+let header = document.getElementById("main-header")
+// console.log(headertitle.textContent = "hello");
+// console.log(headertitle.innerText = "bybye")
+// console.log(headertitle.textContent)
+// console.log(headertitle.innerText);
+
+header.style.borderBottom = "solid 3px #000";
+
+var item = document.getElementsByClassName("title")
