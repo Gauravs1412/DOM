35e6856a6a529f840699b706305aee8cca25ff42
gaurav <gauravradadiya1412@gmail.com>
2022-06-22 15:51:56 +0530

    forth commit

--- dom.js
+++ dom.js
@@ -12,36 +12,69 @@
 //getelementbyId
 
 // console.log(document.getElementById('header-title'));
-let headertitle = document.getElementById('header-title')
-let header = document.getElementById("main-header")
+// let headertitle = document.getElementById('header-title')
+// let header = document.getElementById("main-header")
 // console.log(headertitle.textContent = "hello");
 // console.log(headertitle.innerText = "bybye")
 // console.log(headertitle.textContent)
 // console.log(headertitle.innerText);
 
-header.style.borderBottom = "solid 3px #000";
+// header.style.borderBottom = "solid 3px #000";
 
-var items = document.getElementsByClassName("list-group-item");
-console.log(items);
+// var items = document.getElementsByClassName("list-group-item");
+// console.log(items);
 
-items[2].style.backgroundColor = "green";
+// items[2].style.backgroundColor = "green";
 
-//gives an error need to loop through the list
-//items.style.fontWeight = "bold";
+// //gives an error need to loop through the list
+// //items.style.fontWeight = "bold";
 
-for(let i=1; i<items.length; i++){
-    items[i].style.fontWeight = "bold";
-}
+// for(let i=1; i<items.length; i++){
+//     items[i].style.fontWeight = "bold";
+// }
 
 
 
 //getElementbytagName
 
-var li = document.getElementsByTagName("li")
-console.log(li);
-//it takes all the li tags from the file
-for(let i=0; i<li.length; i++){
-    li[i].style.backgroundColor = "grey";
+// 
+
+
+//queryselsector
+
+// var header = document.querySelector("#main-header")
+// header.style.borderBottom = 'solid 4px red';
+
+// var input = document.querySelector("input")
+// input.value = "hello world"
+
+// var submit = document.querySelector("input[type = 'submit']");
+
+// submit.value = "changed";
+
+//it can only pick one element at a time so be careful
+// var item = document.querySelector(".list-group-item")
+// console.log(item);
+// item.style.color = "green";
+
+// var seconditem = document.querySelector('.list-group-item:nth-child(2)');
+// seconditem.style.backgroundColor = "green";
+
+// var thirditem = document.querySelector('.list-group-item:nth-child(3)')
+// thirditem.style.visibility = "hidden";
+
+//queryselectorAll
+
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+
+titles[1].style.color = "blue";
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+for(let i=0; i<odd.length; i++){
+    odd[i].style.backgroundColor = "green"
 }
+
+
 
 
