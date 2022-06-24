f5e14c22fadea5cd82ef83b6145de405829c18b2
gaurav <gauravradadiya1412@gmail.com>
2022-06-24 19:34:07 +0530

    2nd commit

--- project2.js
+++ project2.js
@@ -4,8 +4,15 @@ function saveData(){
 let name = document.getElementById('name').value;
 let Email = document.getElementById('email').value;
 
-localStorage.setItem("name", name);
-localStorage.setItem("email", Email);
+// localStorage.setItem("name", name);
+// localStorage.setItem("email", Email);
+
+let myObj= {
+    name,
+    Email
+}
+
+localStorage.setItem("userDetails",JSON.stringify(myObj));
 }
 
 
